3e8298a957ff965ca955abfd6cbe5a5619099bc1
saadat-atta-brainx <saadat.atta@brainxtech.com>
2020-02-14 16:24:46 +0500

    Companies controller refactored

--- app/javascript/controllers/companies_controller.js
+++ app/javascript/controllers/companies_controller.js
@@ -1,48 +1,47 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
-import {Controller} from "stimulus"
-import {getMetaValue} from "@rails/activestorage/src/helpers";
+import { Controller } from "stimulus"
+import { getMetaValue } from "@rails/activestorage/src/helpers";
 
 export default class extends Controller {
-    static targets = ["output", "name"]
+  //Convention to follow for output tags. Name of input field+ "Output"
+  static targets = ["nameOutput", "name"]
 
-    addName(event) {
-        let dataId = this.data.element.dataset.id;
-        let companyData = {name: this.nameTarget.value};
-        let ref = this
-        this.UpdateRecord(dataId, companyData)
-            .then(function (response) {
-                if (response.status == 200) {
-                    ref.outputTargets.forEach((target) => {
-                        target.textContent = ref.nameTarget.value
-                        $('#company-name-input').attr('data-name', ref.nameTarget.value)
-                        $('#company-name-input').focus().blur();
-                    });
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
+  onChange(event) {
 
-    UpdateRecord(dataId, companyData) {
-        let response = fetch("/companies/" + dataId, {
-            method: 'PUT',
-            credentials: 'include',
-            dataType: 'JSON',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                "X-CSRF-Token": getMetaValue("csrf-token")
-            }),
-            body: JSON.stringify(companyData),
-        });
-        return response;
-    }
+    let key = event.target.dataset.target.split(".")[1];
+    let dataId = this.data.element.dataset.id;
+    let companyData = {};
+    companyData[key] = this[key + "Target"].value
+    // this.constructor.targets.forEach((key)=>{
+    //     if(this[key+"Target"].dataset.isInput === "true")
+    //         companyData[key] = this[key+"Target"].value
+    // });
+    console.log(companyData);
+    let ref = this;
+    this.UpdateRecord(dataId, companyData)
+      .then(function (response) {
+        if (response.status === 200) {
+          ref[key + "OutputTargets"].forEach((target) => {
+            target.textContent = ref[key + "Target"].value;
+            $(event.target).attr('data-name', ref[key + "Target"].value).focus().blur();
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  UpdateRecord(dataId, companyData) {
+    let response = fetch("/companies/" + dataId, {
+      method: 'PUT',
+      credentials: 'include',
+      dataType: 'JSON',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        "X-CSRF-Token": getMetaValue("csrf-token")
+      }),
+      body: JSON.stringify(companyData),
+    });
+    return response;
+  }
 }
